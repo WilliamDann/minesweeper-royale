d4237a37e0a5977eea2772a410a124a0b5ed1d7a
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2019-03-02 12:39:40 -0800

    Painful Init

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
