a78c7e25cea8f22dc908af6a61e8fa6cdb969992
WilliamDann <wcdann1@gmail.com>
2019-03-02 20:43:30 -0800

    Added minefield funcs

--- backend/mine.test.py
+++ backend/mine.test.py
@@ -2,13 +2,11 @@ from mine import *
 
 t = Minefield(20, 20)
 t.populate(50)
+ret = t.click(10, 10)
+print("\n"*3)
+print(ret)
 
-for x in t.field:
-    for i in range(len(x)):
-        end = " "
-
-        if (i+1 < len(x) and x[i+1] == -1):
-            end = ""
-
-        print(x[i], end=end)
-    print()
+# for x in t.field:
+#     for y in x:
+#         print(y.cleared, end = " ")
+#     print()
