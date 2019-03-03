61616dbb5bd8f9626da5c76e029a94828e2c551f
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2019-03-02 20:54:41 -0800

    Temp Push

---
+++ webhosting/player.py
@@ -0,0 +1,14 @@
+class playerObj:
+    #Arrays for coords and windows size are x,y
+    def __init__(self, websocket):
+        self.coords = []
+        self.windowSize = []
+        self.isReady = False
+        self.isAlive = True
+        self.mouseCoords = []
+        self.name = ''
+        self.lastClick = []
+        
+    
+
+    
