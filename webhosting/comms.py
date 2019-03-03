f3ae3f1ee2f7218d7e40379dcaf2476c2934d3e0
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2019-03-02 21:51:59 -0800

    littlechanges

--- webhosting/comms.py
+++ webhosting/comms.py
@@ -16,15 +16,14 @@ class Comms:
 
     def __init__(self):
         self.data = {}
-        asyncio.get_event_loop().run_until_complete(
-        websockets.serve(handleMsg, 'localhost', 5000))
-        asyncio.get_event_loop().run_forever()
+        
 
     
-    async def handleMsg(self,websocket,path):
-        async for message in websocket:
-            response = self.commandHandler(message,websocket)
-
+    #async def handleMsg(self,websocket,path):
+     #   async for message in websocket:
+     #       response = self.commandHandler(message,websocket)
+    #example socket app
+   
 
    
     def findplayer(self,websocket):
@@ -51,24 +50,21 @@ class Comms:
         currAction = self.data['action']
 
         if(currAction == 'ready'):
-            self.verifyName(self.data['username']) #TODO: Respond with invalid name 
+            assert self.verifyName(self.data['username']) #TODO: Respond with invalid name 
             currPlayer.ready = True
         elif(currAction == 'click'):
             currPlayer.lastClick = [self.data['x'],self.data['y']]
             #TODO: implement click event
 
-    
-        
 
 
-
-    
-       
-
-
-
-
-#example socket app
 async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message + " reply")
+        print("did it")
+
+
+asyncio.get_event_loop().run_until_complete(websockets.serve(echo, 'localhost', 5000))
+asyncio.get_event_loop().run_forever()
+
+
